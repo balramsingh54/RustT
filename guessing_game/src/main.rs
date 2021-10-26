97d0d122c0e6976cc365c7c10288e3436028a1c6
“Balram <balram.singh@startelelogic.com>
2021-10-26 17:34:01 +0530

    variable mutable

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,4 +10,14 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    // by default variable are immutable it means
+    // mutable means : it's value can be changed 
+    // immutable mean: it's value can't be changed
+
+    let mut x= 5;
+    println!("the value of x is {}", x);
+    
+    x= 6;
+    println!("the value of x is {}", x);
 }
