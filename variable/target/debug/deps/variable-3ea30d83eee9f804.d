2786d1a47b5207d01948491b9e31cab0d5e17958
“Balram <balram.singh@startelelogic.com>
2021-10-26 17:38:20 +0530

    variable

---
+++ variable/target/debug/deps/variable-3ea30d83eee9f804.d
@@ -0,0 +1,5 @@
+/home/balram/Desktop/RustT/variable/target/debug/deps/variable-3ea30d83eee9f804: src/main.rs
+
+/home/balram/Desktop/RustT/variable/target/debug/deps/variable-3ea30d83eee9f804.d: src/main.rs
+
+src/main.rs:
---
+++ variable/target/debug/variable.d
@@ -0,0 +1,1 @@
+/home/balram/Desktop/RustT/variable/target/debug/variable: /home/balram/Desktop/RustT/variable/src/main.rs
