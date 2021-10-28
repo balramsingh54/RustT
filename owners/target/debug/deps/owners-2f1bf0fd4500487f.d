9493f3aab67173a0a6831f29b7279883ea2a4640
“Balram <balram.singh@startelelogic.com>
2021-10-28 12:43:52 +0530

    owner

---
+++ owners/target/debug/deps/owners-2f1bf0fd4500487f.d
@@ -0,0 +1,5 @@
+/home/balram/Desktop/RustT/owners/target/debug/deps/owners-2f1bf0fd4500487f: src/main.rs
+
+/home/balram/Desktop/RustT/owners/target/debug/deps/owners-2f1bf0fd4500487f.d: src/main.rs
+
+src/main.rs:
---
+++ owners/target/debug/owners.d
@@ -0,0 +1,1 @@
+/home/balram/Desktop/RustT/owners/target/debug/owners: /home/balram/Desktop/RustT/owners/src/main.rs
