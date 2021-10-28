5703d1de439bf5cd9667a76e268a60a47d40e497
“Balram <balram.singh@startelelogic.com>
2021-10-28 18:22:30 +0530

    ownership

--- owners/src/main.rs
+++ owners/src/main.rs
@@ -1,3 +1,25 @@
+// ownership and functions 
+
+fn take_ownership(s:String){
+    println!("{}",s);
+}
+
+fn move_copy(c:char){
+    println!("{}",c);
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let st=String::from("Balram");
+    take_ownership(st);
+
+    let ch = 'a';
+    move_copy(ch);
+    println!("{}",ch);
+
+    // copy string 
+    let s1= String::from("BalramSingh");
+    let s2=s1;
+    println("{}",s2);
+    
 }
