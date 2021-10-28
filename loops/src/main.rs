adcc2944828777779c35ff295992a9205da5430e
“Balram <balram.singh@startelelogic.com>
2021-10-28 11:03:32 +0530

    loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,26 +1,26 @@
 fn main() {
-    // loop {
-    //     println!("continue printing...");
-    // }
+    loop {
+        println!("continue printing...");
+    }
 
-    // for temp_var in lower value.. max value;
+    for temp_var in lower value.. max value;
 
-    // for x in 1..11{
-    //     if x==5{
-    //         continue;
-    //     }
-    //     println!("the value of x is : {}",x);
-    // }
+    for x in 1..11{
+        if x==5{
+            continue;
+        }
+        println!("the value of x is : {}",x);
+    }
 
-    // loop is also called while loop 
-    // let mut i= 1;
-    // loop {
-    //     println!("this is the value of i {}",i);
-    //     if i==5{
-    //         break;
-    //     }
-    //     i=i+1;
-    // }
+    loop is also called while loop 
+    let mut i= 1;
+    loop {
+        println!("this is the value of i {}",i);
+        if i==5{
+            break;
+        }
+        i=i+1;
+    }
 
     // 1 2 3 4 5 6 7 8 9 10 using for loop
 
