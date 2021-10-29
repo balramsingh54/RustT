792b1dea21bd9bf937caea4d635a6a7283fd4256
“Balram <balram.singh@startelelogic.com>
2021-10-29 15:38:30 +0530

    refer and borrowing

--- ref_borrow/src/main.rs
+++ ref_borrow/src/main.rs
@@ -1,3 +1,22 @@
+//Borrowing allows to have multiple references to a single
+// resource but still obeys to have a "single owner".
+
+fn calculate_length(s:&String)-> usize{
+    s.len()
+}
+
+fn change(strn: &mut String){
+    strn.push_str(" world");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut st= String::from("Balram");
+    let len= calculate_length(&st);
+    println!("size of string is {}", len);
+
+    let mut ss:String = String::from("Hello");
+    println!(" Before modification {}",ss);
+    change(&mut ss);
+    println!("{}",ss);
+
 }
